8505578fd3933c43382ce8eaabe59eafed2c9e3d
perrie_d <111010793+perried@users.noreply.github.com>
2023-03-12 21:37:16 +0200

    Update 6-print_comb3.py
    fixing undefined variable

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(10):
-    for j in range(i + j, 10):
+    for j in range(i + 1, 10):
         if i == 8 and j == 9:
             print("{}{}".format(i, j))
         else:
